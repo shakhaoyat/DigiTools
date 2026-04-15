a39bf4f8aaf3efc2a606cb71802500b45a9cd9ad
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-04-16 02:07:59 +0600

    8th commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import './App.css'
 import NavBer from './components/navBer'
 import Hero from './components/hero'
 import Status from './components/status'
+import Products from './components/products'
 
+
+const fetchProducts = async () => {
+  const rsc = await fetch("/data.json");
+  const data = await rsc.json();
+  return data;
+}
 
 function App() {
-
+  const products = fetchProducts();
   return (
     <>
       <NavBer></NavBer>
       <Hero></Hero>
       <Status></Status>
-
+      <Products></Products>
     </>
   )
 }
